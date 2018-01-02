5789b24a2ad486a4183247f9b435d352749526aa
wozhuchenfu <1107889298@qq.com>
2018-01-02 18:20:51 +0800

    json

--- baseLearn/baseLearn.go
+++ baseLearn/baseLearn.go
@@ -9,6 +9,7 @@ import (
 	"sync"
 	"math/rand"
 	"sort"
+	"encoding/json"
 )
 
 var size = 10
@@ -774,9 +775,50 @@ func sortAction()  {
 	sort.Sort(ByLength(fruits))
 	fmt.Println(fruits)
 }
+//json
+type Response1 struct {
+	Page   int
+	Fruits []string
+}
+
+type Response2 struct {
+	Page int   //'json:"page"'
+	Fruits []string  //'json:"fruits"'
+}
+
+func JsonTest(){
+	bolB,_:=json.Marshal(true)
+	fmt.Println(string(bolB))
+	intB,_:=json.Marshal(1)
+	fmt.Println(string(intB))
+	fltB,_:=json.Marshal(2.34)
+	fmt.Println(string(fltB))
+	slcD:=[]string{"apple","peach","pear"}
+	slcB,_:=json.Marshal(slcD)
+	fmt.Println(slcB)
+	mapD:=map[string]int{"apple":5,"lettuce":7}
+	mapB,_:=json.Marshal(mapD)
+	fmt.Println(mapB)
+
+	res1D:=&Response1{
+		Page:1,
+		Fruits:[]string{"apple","peach","pear"},
+	}
+	res1B,_:=json.Marshal(res1D)
+	fmt.Println(res1B)
+
+	//byt := []byte('{"num":6.33,"strs":["a","b"]}')
+	
+	var dat map[string]interface{}
+	byt := []byte{'a':1,'b':'e'}
+	if err := json.Unmarshal(byt,&dat);err!=nil{
+		panic(err)
+	}
 
 
 
+
+}
 
 
 
