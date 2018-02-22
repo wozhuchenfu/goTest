5d7132993cc201e89c1c02d366b0162b329ac3ed
wozhuchenfu <1107889298@qq.com>
2018-02-22 15:28:03 +0800

    go面试题

--- main.go
+++ main.go
@@ -156,7 +156,56 @@ func main()  {
 		fmt.Println("BBBBBBB")
 		fmt.Println(live())
 	}
-	//var peo People2 = Student2{}//People2与Student2是不同类型的数据不能直接等号赋值
+
+	/*
+	考点：interface内部结构
+解答：
+很经典的题！ 这个考点是很多人忽略的interface内部结构。 go中的接口分为两种一种是空的接口类似这样：
+
+var in interface{}
+另一种如题目：
+
+type People interface {
+    Show()
+}
+他们的底层结构如下：
+
+type eface struct {      //空接口
+    _type *_type         //类型信息
+    data  unsafe.Pointer //指向数据的指针(go语言中特殊的指针类型unsafe.Pointer类似于c语言中的void*)
+}
+type iface struct {      //带有方法的接口
+    tab  *itab           //存储type信息还有结构实现方法的集合
+    data unsafe.Pointer  //指向数据的指针(go语言中特殊的指针类型unsafe.Pointer类似于c语言中的void*)
+}
+type _type struct {
+    size       uintptr  //类型大小
+    ptrdata    uintptr  //前缀持有所有指针的内存大小
+    hash       uint32   //数据hash值
+    tflag      tflag
+    align      uint8    //对齐
+    fieldalign uint8    //嵌入结构体时的对齐
+    kind       uint8    //kind 有些枚举值kind等于0是无效的
+    alg        *typeAlg //函数指针数组，类型实现的所有方法
+    gcdata    *byte
+    str       nameOff
+    ptrToThis typeOff
+}
+type itab struct {
+    inter  *interfacetype  //接口类型
+    _type  *_type          //结构类型
+    link   *itab
+    bad    int32
+    inhash int32
+    fun    [1]uintptr      //可变大小 方法集合
+}
+可以看出iface比eface 中间多了一层itab结构。 itab 存储_type信息和[]fun方法集，从上面的结构我们就可得出，因为data指向了nil 并不代表interface 是nil， 所以返回值并不为空，这里的fun(方法集)定义了接口的接收规则，在编译的过程中需要验证是否实现接口 结果：
+	 */
+
+
+	var peo People2 = Student2{}
+	think := "bitch"
+	fmt.Println(peo.Speak(think))
 	//fmt.Println(size,max_size)
 	//常量不同于变量的在运行期分配内存，常量通常会被编译器在预处理阶段直接展开，作为指令数据使用，
 	//println(&cl,cl)
@@ -555,8 +604,17 @@ func exceptionTest()  {
 	case value :=<-int_chan:
 		fmt.Println(value)
 	case value := <-string_chan:
-		panic(value)//抛出异常,当程序运行时，如果遇到引用空指针、下标越界或显式调用panic函数等情况，则先触发panic函数的执行，然后调用延迟函数。
+		panic(value)
 	}
+	/*
+	考点：select随机性
+	解答：
+	select会随机选择一个可用通道做收发操作。 所以代码是有可能触发异常，也有可能不会。 单个chan如果无缓冲时，将会阻塞。但结合 select可以在多个chan间等待执行。有三点原则：
+
+	select 中只要有一个case能return，则立刻执行。
+	当如果同一时间有多个case均能return则伪随机方式抽取任意一个执行。
+	如果没有一个case能return则可以执行”default”块。
+	 */
 }
 
 //考察defer 和函数内部调用返回值
